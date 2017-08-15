de98661b973b854f377f781f1d84ab43600e7846
eukaryote <minerguy31@gmail.com>
2017-08-15 12:59:03 -0600

    Add textual explanation to enum value

--- src/main/java/eukaryote/iota/confstat/Status.java
+++ src/main/java/eukaryote/iota/confstat/Status.java
@@ -4,25 +4,36 @@ public enum Status {
 	/**
 	 * Transaction has inclusion in latest milestone
 	 */
-	CONFIRMED,
+	CONFIRMED("Confirmed"),
 	
 	/**
 	 * Transaction waiting to be reviewed and referenced via new transaction
 	 */
-	PENDING,
+	PENDING("Pending"),
 	
 	/**
 	 * Double spent failed transaction and will never be confirmed
 	 */
-	DOUBLESPEND,
+	DOUBLESPEND("Failed - Double Spent"),
 	
 	/**
 	 * Depth too low in network and needs to be reattached
 	 */
-	DEPTHLOW,
+	DEPTHLOW("Failed - Reattach Needed"),
 	
 	/**
 	 * Transaction has invalid trytes
 	 */
-	INVALID
+	INVALID("Failed - Invalid");
+	
+	String name;
+	
+	Status(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
 }
