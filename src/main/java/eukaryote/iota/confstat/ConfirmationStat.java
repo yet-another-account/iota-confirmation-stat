656febd4df20e40e836fdf8c708777cda068ab71
eukaryote <minerguy31@gmail.com>
2017-08-15 13:23:15 -0600

    Add doublespend check

--- src/main/java/eukaryote/iota/confstat/ConfirmationStat.java
+++ src/main/java/eukaryote/iota/confstat/ConfirmationStat.java
@@ -1,23 +1,23 @@
 package eukaryote.iota.confstat;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import jota.IotaAPI;
+import jota.dto.response.GetBalancesResponse;
 import jota.dto.response.GetBundleResponse;
 import jota.dto.response.GetInclusionStateResponse;
 import jota.error.ArgumentException;
 import jota.error.InvalidBundleException;
 import jota.error.InvalidSignatureException;
 import jota.error.NoNodeInfoException;
-import jota.model.Bundle;
 import jota.model.Transaction;
 
 public class ConfirmationStat {
 	IotaAPI api;
+	
+	// empty transaction array
+	private static final Transaction[] txnarr = {};
 
 	public ConfirmationStat(IotaAPI api) {
 		this.api = api;
@@ -46,6 +46,48 @@ public class ConfirmationStat {
 			
 			// bad bundle?
 			return Status.INVALID;
+		}
+		
+		// check for doublespending
+		
+		List<Transaction> inputs = new ArrayList<>(bdl.getTransactions().size());
+		
+		for (Transaction t : bdl.getTransactions()) {
+			// we only want inputs
+			if (t.getValue() >= 0)
+				continue;
+			
+			inputs.add(t);
+		}
+		
+		// look up address balances and compare
+		
+		Transaction[] inparray = inputs.toArray(txnarr);
+		String[] addresses = new String[inparray.length];
+		String[] txnhashes = new String[inparray.length];
+		boolean[] inclusion = api.getLatestInclusion(txnhashes).getStates();
+		
+		for (int i = 0; i < inparray.length; i++) {
+			addresses[i] = inparray[i].getAddress();
+			txnhashes[i] = inparray[i].getHash();
+		}
+		
+		GetBalancesResponse balances = api.getBalances(1, addresses);
+		String[] balstrs = balances.getBalances();
+		
+		
+		for (int i = 0; i < balstrs.length; i++) {
+			String balance = balstrs[i];
+			long bal = Long.parseLong(balance);
+			
+			// if not confirmed, we ignore
+			if (inclusion[i] == false)
+				continue;
+			
+			// invert value because input balance is negative
+			if (bal < -inparray[i].getValue()) {
+				return Status.DOUBLESPEND;
+			}
 		}
 		
 		return Status.PENDING;
